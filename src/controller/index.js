05de8d553331433bf76f7fb28be2d2bdb598592e
dinanjana <dinanjana@fixico.nl>
2021-06-03 08:32:03 +0530

    removed unwanted logs

--- src/controller/index.js
+++ src/controller/index.js
@@ -5,7 +5,6 @@ const controllers = [{
     path: '/ledger',
     method: 'get',
     handlers: [ValidationService.getLedger, (req, res) => {
-        console.log(`${process.pid} handling the request`);
         const terms = LedgerService.getLedger(req.query);
         res.status(200);
         res.send(terms);
